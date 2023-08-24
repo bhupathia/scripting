9394c339e50fa3aecf00a727eae72959cc2e93c3
bhupathi <bhupathinaidu.bpn@gmail.com>
2023-08-24 14:14:37 +0530

    added

--- create-ec2.sh
+++ create-ec2.sh
@@ -3,16 +3,17 @@ NAMES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping"
 INSTANCE_TYPE=""
 IMAGE_ID=ami-03265a0778a880afb
 SECURITY_GROUP_ID=sg-0eabcbb9daa289571
+
 for i in "${NAMES[@]}"
 do
   if [[ $i == "mongodb" || $i == "mysql" ]]
   then
-  INSTANCE_TYPE="t3.medium"
+     INSTANCE_TYPE="t3.medium"
   else
-   INSTANCE_TYPE="t2.micro"
+     INSTANCE_TYPE="t2.micro"
   fi 
  echo "creating : $i instance"
-  IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID  --instance-type t2.micro  --security-group-ids  $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" |jq -r 'Instances[0].PrivateIpAddress')
+  IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID  --instance-type t2.micro  --security-group-ids  $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r 'Instances[0].PrivateIpAddress')
    echo "creating : $i instance : $IP_ADDRESS"
 
 done 
